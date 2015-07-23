b854a9e1fbf68eb611a86a603e53ea41c2637a86
Stacey Maresco <stacey.maresco@gmail.com>
2015-07-22 22:05:28 -0400

    figuring out operations

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var currentScreen = "";
+var callbacks = $.Callbacks();
 
 $(document).ready(function(){
     $(".numbers").click(function(e) {
       currentScreen = currentScreen + e.currentTarget.textContent;
       $("th").text(currentScreen);
       console.log(currentScreen);
+    });
+    $(".plus").click(function() {
+      callbacks.add(currentScreen);
+      callbacks.fire("world");
     });
 
 });
